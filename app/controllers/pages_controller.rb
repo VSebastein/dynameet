95be1fe659cece1a6c63f62bbcf8ac4ac95a9ef6
vsebastien <sebastien.vincendeau@gmail.com>
2015-09-17 22:57:34 +0200

    Finished basic static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
   def home
+  	@titre = "Enhance your meetings"
   end
 
   def contact
+  	@titre = "Contact"
   end
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
 
+  describe PagesController do
+    render_views
+
+  end
+
   describe "GET #home" do
     it "returns http success" do
       get :home
@@ -15,5 +20,6 @@ RSpec.describe PagesController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
 
 end
