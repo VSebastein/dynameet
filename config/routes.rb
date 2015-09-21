f9acb016a82c0c3aaa75836ca1ed11cd78183ee6
vsebastien <sebastien.vincendeau@gmail.com>
2015-09-21 21:59:23 +0200

    Added users controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
+
   # Homepage
   root 'pages#home'
   # Contact page
   get 'contact' => 'pages#contact'
+
+  # Signup Page
+  get 'signup' => 'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
